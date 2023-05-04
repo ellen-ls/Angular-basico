9fb2cb25c8aef587b35382cd7cb6c05834cd3d00
ellen-ls <ellen.l.s1996@gmail.com>
2023-05-03 22:17:03 -0300

    estruturando o big card

--- Angular-blog/src/app/app.module.ts
+++ Angular-blog/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardsComponent } from './components/small-cards/small-cards.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SmallCardsComponent } from './components/small-cards/small-cards.compon
     MenuBarComponent,
     MenuTitleComponent,
     BigCardComponent,
-    SmallCardsComponent
+    SmallCardsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
