299e825a2987cb9ee754b60f84e7e8677807e011
ellen-ls <ellen.l.s1996@gmail.com>
2023-05-06 15:33:16 -0300

    projeto concluido

--- Angular-blog/src/app/data/dataFake.ts
+++ Angular-blog/src/app/data/dataFake.ts
@@ -1,17 +1,33 @@
 export const dataFake=[
     {
-        "id":1,
-        "photo":"aaa",
-        "title":"Ola",
-        "description":"ola mundo"
+        "id":"1",
+        "title":"Minha noticia",
+        "description":"Minha noticia",
+        "photoCover":"https://ae01.alicdn.com/kf/HTB1jmbvpYuWBuNjSszgq6z8jVXaF/Plano-de-fundo-de-parede-para-fotografia-floresta-desenho-animado-glade-material-de-poli-ster-ou.jpg_Q90.jpg_.webp"
 
     },
 
     {
-        "id":2,
-        "photo":"aaa",
+        "id":"2",
+        "title":"Viva e aprenda",
+        "description":"Viva e aprenda",
+        "photoCover":"https://amplino.org/wp-content/uploads/-000/1/82580c2a6c37339.jpg"
+
+    },
+
+    {
+        "id":"3",
         "title":"Ola",
-        "description":"ola mundo"
+        "description":"Aprecie a natureza",
+        "photoCover":"https://a-static.mlcdn.com.br/800x560/quadro-decorativo-lua-noite-lago-arvores-paisagem-natureza-decoracoes-com-moldura-g2-vital-quadros/vitalquadrosdobrasil/90990601/b0b3d67a05b68370626ab39681f911a0.jpeg"
+
+    },
+
+    {
+        "id":"4",
+        "title":"Ola",
+        "description":"Respire",
+        "photoCover":"https://a-static.mlcdn.com.br/800x560/adesivo-parede-paisagem-praia-mar-sol-verao-natureza-gg372-quartinho-decorado/oliststore/mgljo49sy6stgmvg/0e26bea510ce9b0164247c2ae06257ef.jpeg"
 
     }
 
--- Angular-blog/src/app/pages/content/content.component.ts
+++ Angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -9,9 +10,24 @@ export class ContentComponent implements OnInit {
   photoCover:string="https://i0.wp.com/techwek.com/wp-content/uploads/2021/01/lisa-debochada.jpg?resize=576%2C1024&ssl=1"
   contentTitle:string="Minha noticia"
   contentDescription:string=''
-  constructor() { }
+  private id:string | null = "0"
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+      )
+      this.setValuestoComponent(this.id)
   }
+setValuestoComponent(id:string | null){
+const result = dataFake.filter(article =>article.id === id)[0]
 
+
+this.contentTitle = result.title
+this.contentDescription = result.description
+this.photoCover = result.photoCover
+
+}
 }
